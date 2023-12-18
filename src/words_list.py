2797e41f3267b81e8479941ad93e3de2a684948e
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-18 19:29:55 +1100

    Add word list

---
+++ src/words_list.py
@@ -0,0 +1,9 @@
+animals = ["fish", "bird", "giraffe", "penguin", "whale", "shark", "sheep", "crab", "dog", "platypus", "snake", "lizard", "frog", "turtle", "cat", "monkey", "hyena", "octopus", "goat", "kangaroo", "tiger", "lion", "panda", "rabbit", "mouse", "hedgehog", "badger", "bee", "koala", "crocodile"]
+
+countries = ["France", "America", "China", "Spain", "Australia", "Italy", "Germany", "Japan", "Korea", "Canada", "Singapore", "Greece", "Egypt", "India", "Brazil", "Portugal", "Afghanistan", "Colombia", "Malaysia", "Peru", "Norway", "Finland", "Sweden", "Thailand", "Philippines"]
+
+foods = ["burger", "sandwich", "apple", "eggs", "bacon", "sausage", "pizza", "banana", "steak", "spaghetti", "chips", "strawberry", "curry", "burrito", "cake", "sushi", "potato", "carrot", "cucumber", "lobster", "oysters", "muffin", "hashbrown", "rice", "soup", "beans"]
+
+sports = ["golf", "fishing", "soccer", "football", "basketball", "hockey", "track", "volleyball", "swimming", "rugby", "tennis", "surfing", "skiing", "cheerleading", "lacrosse", "rowing", "boxing"]
+
+words_list = animals + countries + foods + sports
