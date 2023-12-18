46ee1c26f4cc6be0149845719e34f8455d5763e6
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-18 20:14:50 +1100

    lowercased all words in list

--- src/words_list.py
+++ src/words_list.py
@@ -1,7 +1,7 @@
 # list of animals
 animals = ["fish", "bird", "giraffe", "penguin", "whale", "shark", "sheep", "crab", "dog", "platypus", "snake", "lizard", "frog", "turtle", "cat", "monkey", "hyena", "octopus", "goat", "kangaroo", "tiger", "lion", "panda", "rabbit", "mouse", "hedgehog", "badger", "bee", "koala", "crocodile"]
 # list of countries
-countries = ["France", "America", "China", "Spain", "Australia", "Italy", "Germany", "Japan", "Korea", "Canada", "Singapore", "Greece", "Egypt", "India", "Brazil", "Portugal", "Afghanistan", "Colombia", "Malaysia", "Peru", "Norway", "Finland", "Sweden", "Thailand", "Philippines"]
+countries = ["france", "america", "china", "spain", "australia", "italy", "germany", "japan", "korea", "canada", "singapore", "greece", "egypt", "india", "brazil", "portugal", "afghanistan", "colombia", "malaysia", "peru", "norway", "finland", "sweden", "thailand", "philippines"]
 # list of foods
 foods = ["burger", "sandwich", "apple", "eggs", "bacon", "sausage", "pizza", "banana", "steak", "spaghetti", "chips", "strawberry", "curry", "burrito", "cake", "sushi", "potato", "carrot", "cucumber", "lobster", "oysters", "muffin", "hashbrown", "rice", "soup", "beans"]
 # list of sports
