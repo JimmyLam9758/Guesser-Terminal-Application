bdc63687d4a5d552ec110271caab396ca91161fc
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-18 20:01:41 +1100

    fix words list

--- src/words_list.py
+++ src/words_list.py
@@ -1,9 +1,10 @@
+# list of animals
 animals = ["fish", "bird", "giraffe", "penguin", "whale", "shark", "sheep", "crab", "dog", "platypus", "snake", "lizard", "frog", "turtle", "cat", "monkey", "hyena", "octopus", "goat", "kangaroo", "tiger", "lion", "panda", "rabbit", "mouse", "hedgehog", "badger", "bee", "koala", "crocodile"]
-
+# list of countries
 countries = ["France", "America", "China", "Spain", "Australia", "Italy", "Germany", "Japan", "Korea", "Canada", "Singapore", "Greece", "Egypt", "India", "Brazil", "Portugal", "Afghanistan", "Colombia", "Malaysia", "Peru", "Norway", "Finland", "Sweden", "Thailand", "Philippines"]
-
+# list of foods
 foods = ["burger", "sandwich", "apple", "eggs", "bacon", "sausage", "pizza", "banana", "steak", "spaghetti", "chips", "strawberry", "curry", "burrito", "cake", "sushi", "potato", "carrot", "cucumber", "lobster", "oysters", "muffin", "hashbrown", "rice", "soup", "beans"]
-
+# list of sports
 sports = ["golf", "fishing", "soccer", "football", "basketball", "hockey", "track", "volleyball", "swimming", "rugby", "tennis", "surfing", "skiing", "cheerleading", "lacrosse", "rowing", "boxing"]
-
-words_list = animals + countries + foods + sports
+# grouping all lists
+words = animals + countries + foods + sports
