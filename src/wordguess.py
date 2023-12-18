3cfa15f99adb5e79bc755493ecb8a7ab6d38fc8a
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-18 23:54:41 +1100

    Add Greeting Input

--- src/wordguess.py
+++ src/wordguess.py
@@ -4,7 +4,11 @@ from words_list import words, animals, foods, countries
 # choosing a randon word from our list
 random_word = random.choice(words)
 
-print(random_word)
+name = input("Enter your name: ").title()
+
+print(f"Welcome {name} to the word guessing game!!!")
+
+print(random_word.title())
 
 if random_word in animals:
     print("Hint: The word is an animal.")
@@ -15,3 +19,10 @@ elif random_word in countries:
 else:
     print("Hint: The word is a sport.")
 
+def create_menu():
+    print("1. Enter 1 start game")
+    print("2. Enter 2 to view word lists")
+    print("3. Enter 3 to view score")
+    print("4. Enter 4 to exit")
+    choice = input("Enter your selection: ")
+    return choice
