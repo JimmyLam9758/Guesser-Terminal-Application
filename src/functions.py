903fcf674087130aa7bde6015af4acb9ad549154
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-19 14:51:06 +1100

    Add functions file

---
+++ src/functions.py
@@ -0,0 +1,7 @@
+def create_menu():
+    print("1. Enter 1 start game")
+    print("2. Enter 2 to view word lists")
+    print("3. Enter 3 to view score")
+    print("4. Enter 4 to exit")
+    choice = input("Enter your selection: ")
+    return choice
