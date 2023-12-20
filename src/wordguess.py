864fb0f6b8c524fb7cf7960dcd2fde10f4549daa
JimmyLam9758 <JimmyLam9758@gmail.com>
2023-12-20 20:54:37 +1100

    Add game restart function

--- src/wordguess.py
+++ src/wordguess.py
@@ -17,90 +17,100 @@ while True:
         print("\nInput has been interrupted, Exiting ")
         break
 
-print(f"\nWelcome {name} to the word guessing game!!!\n")
+print(f"\nWelcome {name} to the word guessing game!!!")
 
-# Game start
-while True:
+# Game start loop
 
-    def game_start():
-        # choosing a randon word from our list and getting its length
-        words = animals + countries + foods + sports
-        random_word = random.choice(words)
-        word_length = len(random_word)
+def game_start():
+    # choosing a randon word from our list and getting its length
+    words = animals + countries + foods + sports
+    random_word = random.choice(words)
+    word_length = len(random_word)
 
-        print(f"\nGame has started, begin Guessing!! Your word is {word_length} letters long.\n")
-        users_guess = ""
-        guesses = 7
+    print(f"\nGame has started, begin Guessing!! Your word is {word_length} letters long.\n")
+    users_guess = ""
+    guesses = 7
 
-        # print correctly guessed characters
-        while guesses > 0:
-            incorrect_guesses = 0
-            for str in random_word:
-                if str in users_guess:
-                    print(str, end = " ")
-                else:
-                    incorrect_guesses += 1
-                    print("_", end = " ")
-
-            # Print if word was guessed
-            if incorrect_guesses == 0:
-                print(f"\n\nCongratulations! You got it right with {guesses} guesses remaining. The word was {random_word}.\n")
-                # play again option if word was guessed
-                start_again = input("Would you like to play again? Y/N: ").strip()
-                if start_again == "Y":
-                    break
-                else:
-                    print(f"\nThank you {name} for playing! Come back soon!")
-                    quit()
-
-            # User input for guess
-            guess = input("\n\nEnter a letter: ").lower()
-
-            # Make sure guess is 1 only character long and in the english alphabet.
-            if len(guess) != 1 or not guess.isalpha():
-                print("\nInvalid Input. Please enter 1 letter.")
-                continue
-
-            # add guessed letter to their guesses
-            users_guess += guess
-
-            # if incorrect guess, lower total guess count, print remaining guesses.
-            if guess not in random_word:
-                guesses -= 1
-                print(f"\nYou have {guesses} guesses remaining.\n")
-                
-                # At certain amount of guesses print hint
-                if guesses == 4:
-                        if random_word in animals:
-                            print("\nHint: The word is an animal.\n")
-                        elif random_word in foods:
-                            print("\nHint: The word is a food item.\n")
-                        elif random_word in countries:
-                            print("\nHint: The word is a country.\n")
-                        else:
-                            print("\nHint: The word is a sport.\n")
-                if guesses == 0:
-                    print(f"\nBad luck, You have run out of guesses. The word was {random_word}.")
-                    restart = input("\nWould you like to play again? Y/N: ")
-                    if restart == "N":
-                        print(f"\nThank you {name} for playing! Come back soon!")
+    # print correctly guessed characters
+    while guesses > 0:
+        incorrect_guesses = 0
+        for str in random_word:
+            if str in users_guess:
+                print(str, end = "")
+            else:
+                incorrect_guesses += 1
+                print("_", end = "")
+        # restart function
+        def restart():
+                while True:
+                    start_again = input("\nWould you like to play again? Y/N: ").upper()
+                    if start_again == "Y":
+                        game_start()
+                    elif start_again == "N":
                         quit()
+                    else:
+                        print("\nInvalid Input. Back to main menu.")
+                        return
+
+        # Print if word was guessed
+        if incorrect_guesses == 0:
+            print(f"\n\nCongratulations! You got it right with {guesses} guesses remaining. The word was {random_word}.\n")
+            # play again option if word was guessed
+            restart()
+            break
+
+        # User input for guess
+        guess = input("\n\nEnter a letter: ").lower()
+
+        # Make sure guess is 1 only character long and in the english alphabet.
+        if len(guess) != 1 or not guess.isalpha():
+            print("\nInvalid Input. Please enter a single letter.")
+            continue
+
+        # add guessed letter to their guesses
+        users_guess += guess
+
+        # if incorrect guess, lower total guess count, print remaining guesses.
+        if guess not in random_word:
+            guesses -= 1
+            print(f"\nYou have {guesses} guesses remaining.\n")
+            
+            # At certain amount of guesses print hint
+            if guesses == 4:
+                    if random_word in animals:
+                        print("\nHint: The word is an animal.\n")
+                    elif random_word in foods:
+                        print("\nHint: The word is a food item.\n")
+                    elif random_word in countries:
+                        print("\nHint: The word is a country.\n")
+                    else:
+                        print("\nHint: The word is a sport.\n")
+            # Run out of guesses, game over.
+            if guesses == 0:
+                print(f"\nBad luck, You have run out of guesses. The word was {random_word}.")
+                restart()
 
 # Starting menu
-    file_name = "rules.txt"
-    users_choice = ""
-    while users_choice != "5":
-        users_choice = create_menu()
-        if (users_choice == "1"):
-            game_start()
-        elif (users_choice == "2"):
-            view_wordlist()
-        elif (users_choice == "3"):
-            print("view previous scores")
-        elif(users_choice == "4"):
-            view_rules(file_name)
-        elif (users_choice == "5"):
-            print("See you next time!")
-            quit()
-        else:
-            print("Not a valid Input, Please enter a value between 1-5.\n")
+file_name = "rules.txt"
+users_choice = ""
+while users_choice != "5":
+    users_choice = create_menu()
+    if (users_choice == "1"):
+        game_start()
+    elif (users_choice == "2"):
+        view_wordlist()
+    elif (users_choice == "3"):
+        print("view previous scores")
+    elif(users_choice == "4"):
+        view_rules(file_name)
+    elif (users_choice == "5"):
+        print("See you next time!")
+        quit()
+    else:
+        print("Not a valid Input, Please enter a value between 1-5.\n")
+
+while True:
+    game_start()
+    play_again = input("Would you like to play again? Y/N: ").upper()
+    if play_again == "N":
+        print(f"Thanks {name} for playing! Goodbye!")
